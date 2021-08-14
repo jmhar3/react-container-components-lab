87283d02310f2a7e2c0b39877331eb8d61c29a15
jmhar <jmhar@protonmail.com>
2021-08-14 10:03:43 +1000

    basic js structure

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (array) => {
+    return(
+        <div className="review-list">
+            {array.forEach(element => {
+                return(<div className="review">{element}</div>)
+            })}
+        </div>
+    )
+}
+
+export default MovieReviews
